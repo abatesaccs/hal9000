2cee58dd82b4f724a6930152930dfd78c0f6f254
Andrew <abatesaccs@gmail.com>
2019-11-01 10:52:40 -0700

    create html page to get input

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var hal9000 = new function(){
+
+}
+
+
+$(document).ready(function() {
+  $("form#input-string").submit(function(event) {
+    event.preventDefault();
+    var string = parseInt($("input#inputString").val());
+    console.log(string);
+    var result = hal9000(string);
+  });
+});
