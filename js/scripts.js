d8c855f33ad72ec354dc40cc588f62faa496de40
Andrew <abatesaccs@gmail.com>
2019-11-01 12:46:28 -0700

    appends the input and output below the submit button

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,18 @@ var hal9000 = function(num){
   var resultsArray = new Array();
 
   for(i = 0; i <= num; i++){
-    resultsArray[i] = i;
+    resultsArray[i] = i + " ";
     var holder = resultsArray[i].toString();
     if (holder.includes(3)){
-      resultsArray[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
+      resultsArray[i] = "I'm sorry, Dave. I'm afraid I can't do that. ";
     } else if (holder.includes(2)){
-      resultsArray[i] = "Boop";
+      resultsArray[i] = "Boop ";
     } else if (holder.includes(1)) {
-      resultsArray[i] = "Beep";
+      resultsArray[i] = "Beep ";
     }
   }
   console.log(resultsArray);
+  return resultsArray;
 }
 
 
@@ -22,5 +23,7 @@ $(document).ready(function() {
     var string = parseInt($("input#inputString").val());
     console.log(string);
     var result = hal9000(string);
+
+    $(".results").append(string + ":  " + result + "<br />");
   });
 });
