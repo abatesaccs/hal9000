46976802de1639f791c1b1adc7fff6e50c6c9252
Andrew <abatesaccs@gmail.com>
2019-11-01 12:28:32 -0700

    fulfill the fifth spec

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,12 @@ var hal9000 = function(num){
   for(i = 0; i <= num; i++){
     resultsArray[i] = i;
     var holder = resultsArray[i].toString();
-    if (holder.includes(1)){
-      resultsArray[i] = "Beep";
+    if (holder.includes(3)){
+      resultsArray[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
     } else if (holder.includes(2)){
       resultsArray[i] = "Boop";
-    } else if (holder.includes(3)) {
-      resultsArray[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
+    } else if (holder.includes(1)) {
+      resultsArray[i] = "Beep";
     }
   }
   console.log(resultsArray);
