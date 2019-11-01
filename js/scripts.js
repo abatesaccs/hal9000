20890b9fafb452b4eb192db51731bd9540a4a7ba
Andrew <abatesaccs@gmail.com>
2019-11-01 13:09:55 -0700

    add name field and enter name into response

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-var hal9000 = function(num){
+var hal9000 = function(num, name){
   var resultsArray = new Array();
 
   for(i = 0; i <= num; i++){
     resultsArray[i] = i + " ";
     var holder = resultsArray[i].toString();
     if (holder.includes(3)){
-      resultsArray[i] = "I'm sorry, Dave. I'm afraid I can't do that. ";
+      resultsArray[i] = "I'm sorry, " + name + ". I'm afraid I can't do that. ";
     } else if (holder.includes(2)){
       resultsArray[i] = "Boop ";
     } else if (holder.includes(1)) {
@@ -20,11 +20,13 @@ var hal9000 = function(num){
 $(document).ready(function() {
   $("form#input-string").submit(function(event) {
     event.preventDefault();
-    var string = parseInt($("input#inputString").val());
-    console.log(string);
-    var result = hal9000(string);
+    var num = parseInt($("input#inputNum").val());
+    var name = $("input#inputName").val();
+    console.log(num);
+    console.log(name);
+    var result = hal9000(num, name);
 
-    $(".results").append(string + ":  " + result + "<br />");
+    $(".results").append(num + ":  " + result + "<br />");
   });
   $("form#clear").click(function(event) {
     event.preventDefault();
